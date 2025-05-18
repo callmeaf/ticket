dd57c0965652e92159a1586974122d331ea7ba57
callmeaf <alikhoshkar2000@gmail.com>
2025-05-18 11:32:20 +0330

    change email template

--- resources/views/admin/v1/mail/tickets/status_changed.blade.php
+++ resources/views/admin/v1/mail/tickets/status_changed.blade.php
@@ -1,13 +1,13 @@
 <x-mail::message>
-    # {{ __('callmeaf-ticket::admin_v1.mail.status_changed.title', ['ref_code' => $ref_code]) }}
+# {{ __('callmeaf-ticket::admin_v1.mail.status_changed.title', ['ref_code' => $ref_code]) }}
 
-    {{ __('callmeaf-ticket::admin_v1.mail.status_changed.body', ['title' => $title,'status_text' => $status_text]) }}
+{{ __('callmeaf-ticket::admin_v1.mail.status_changed.body', ['title' => $title,'status_text' => $status_text]) }}
 
-    @component('mail::button', ['url' => $url])
-        {{ __('callmeaf-ticket::admin_v1.mail.status_changed.button') }}
-    @endcomponent
+@component('mail::button', ['url' => $url])
+{{ __('callmeaf-ticket::admin_v1.mail.status_changed.button') }}
+@endcomponent
 
-    {{__('callmeaf-ticket::admin_v1.mail.status_changed.footer')}}
+{{__('callmeaf-ticket::admin_v1.mail.status_changed.footer')}}
 
-    {{ config('app.name') }}
+{{ config('app.name') }}
 </x-mail::message>
--- resources/views/api/v1/mail/tickets/created.blade.php
+++ resources/views/api/v1/mail/tickets/created.blade.php
@@ -1,13 +1,13 @@
 <x-mail::message>
-    # {{ __('callmeaf-ticket::api_v1.mail.created.title', ['ref_code' => $ref_code]) }}
+# {{ __('callmeaf-ticket::api_v1.mail.created.title', ['ref_code' => $ref_code]) }}
 
-    {{ __('callmeaf-ticket::api_v1.mail.created.body', ['title' => $title]) }}
+{{ __('callmeaf-ticket::api_v1.mail.created.body', ['title' => $title]) }}
 
-    @component('mail::button', ['url' => $url])
-        {{ __('callmeaf-ticket::api_v1.mail.created.button') }}
-    @endcomponent
+@component('mail::button', ['url' => $url])
+{{ __('callmeaf-ticket::api_v1.mail.created.button') }}
+@endcomponent
 
-    {{__('callmeaf-ticket::api_v1.mail.created.footer')}}
+{{__('callmeaf-ticket::api_v1.mail.created.footer')}}
 
-    {{ config('app.name') }}
+{{ config('app.name') }}
 </x-mail::message>
